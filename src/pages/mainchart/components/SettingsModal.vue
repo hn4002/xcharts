729afa14272aa9b4862c050e778d94818b37403e
MrHari <hn4002@github.com>
2020-05-21 14:49:52 -0700

    latest code

---
+++ src/pages/mainchart/components/SettingsModal.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-modal
+            id="modal-settings"
+            @show="resetModal"
+            @hidden="resetModal"
+            @ok="handleOk"
+            scrollable
+            title="Settings"
+            size="lg"
+    >
+        <SettingsPanel></SettingsPanel>
+    </b-modal>
+</template>
+
+<script>
+    import SettingsPanel from "./SettingsPanel.vue";
+
+    export default {
+        name: "SettingsModal",
+
+        components: {SettingsPanel},
+
+        methods: {
+            resetModal() {
+                this.$xEventBus.$emit('settingsPanel.reset')
+            },
+
+            handleOk(bvModalEvt) {
+                this.$xEventBus.$emit('settingsPanel.save')
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
